db9e8656186b3de658ec7dce192861b36e66bb39
myersr <rm2@email.sc.edu>
2016-04-08 13:48:21 -0400

    logical logins and query playlsts

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,111 +1,195 @@
 angular.module('starter.controllers', [])
 
-    .controller('AppCtrl', function($scope, $rootScope, $ionicModal, $timeout,$log, $ionicLoading, $q, $http, $location,$state, Spotify, authenticationFact) {
+  .controller('indexController', function($scope, $log, $q, $state, $ionicLoading, authenticationFact, playlistsFact){
+    $scope.showLoading = function() {
+      $ionicLoading.show({
+        template: '<i class="ion-loading-c">Fetching User Account</i>',
+        noBackdrop: false
+      });
+    }
 
-      // With the new view caching in Ionic, Controllers are only called
-      // when they are recreated or on app start, instead of every page change.
-      // To listen for when this page is active (for example, to refresh data),
-      // listen for the $ionicView.enter event:
-      var showLoading = function() {
-        $ionicLoading.show({
-          template: '<i class="ion-loading-c"></i>',
-          noBackdrop: true
-        });
-      }
+    $scope.hideLoading = function() {
+      $ionicLoading.hide();
+    }
+    $scope.load = function () {
+      //alert(window.location.hash);
 
-      var hideLoading = function() {
-        $ionicLoading.hide();
-      }
-
-      var currentState;
-      $scope.$on('$ionicView.enter', function() {
-        // Code you want executed every time view is opened
-        currentState = $state.current.data.link;
-
-        if(!authenticationFact.isAuthorized() || !authenticationFact.hasToken()){
-          showLoading();
-          $log.log("inside token length")
-          var token = authenticationFact.getToken()
+      window.onload = function () {
+        var hash = window.location.hash;
+        //$log.log("hash",hash)
+        if(!hash){
+          $state.go('app.playlists')
+        }else if (hash.includes("access_token")) {
+          $scope.showLoading();
+          // login success
+          //$log.log("Login Success")
+          var token = window.location.hash.split('&')[0].split('=')[1];
           authenticationFact.setToken(token)
-          $log.log($scope.accountInfo)
-          hideLoading()
-        }else{
-          $log.log("good to go")
+          //localStorage.setItem('spotify-token', token);
+          if(!authenticationFact.isAuthorized()){
+            var tken = authenticationFact.getToken()
+            var promise = authenticationFact.queryData(token);
+            promise.then(function(response){
+              $log.log(response)
+              $scope.hideLoading();
+              $state.go("app.playlists")
+              //var promise2 = playlistsFact.getPlaylists();
+              //promise2.then(function(response){
+              //  $log.log(response)
+              //  $state.go("app.playlists", {}, { reload: true })
+              //})
+              //window.location.assign("http://localhost:8100/#/app/playlists")
+            })
+
+          }
+          //$log.log("redirecting")
+
+        }else if (hash.includes('error')){// && !hash.includes('/login')) {
+          $log.log("failing in first if")
+          // login failure
+          window.location.assign("http://localhost:8100/#/login");
         }
-        $scope.accountInfo = authenticationFact.getData();
-        console.log($scope.accountInfo.images[0]);
+      }
+    }
+  })
+
+  .controller('AppCtrl', function($scope, $rootScope, $ionicModal, $timeout,$log, $ionicLoading, $q, $http, $location,$state, Spotify, authenticationFact) {
+
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    $scope.showLoading = function() {
+      $ionicLoading.show({
+        template: '<i class="ion-loading-c"></i>',
+        noBackdrop: true
+      });
+    }
+    $scope.hideLoading = function() {
+      $ionicLoading.hide();
+    }
+
+    var currentState;// = $state.current.data.link;;
+    //$log.log("current State: ",currentState);
+    //$log.log("controller Data log: ", authenticationFact.getData())
+    //event.preventDefault(); //This will cancel the transition
+    // transitionTo() promise will be rejected with
+    // a 'transition prevented' error
+    //$rootScope.$on('$stateChangeSuccess',
+    $rootScope.$on('$stateChangeSuccess',
+      function(event, toState, toParams, fromState, fromParams, options){
+        currentState = toState.data['link'];
+        $log.log("Changed")
+
 
       })
+    //$rootScope.$on("ionicView.beforeEnter", function(){
+    //  $log.log("help")
+    //})
+
+    //if(!authenticationFact.isAuthorized() && authenticationFact.hasToken())
+    //{
+    //
+    //  //var defer = $q.defer();
+    //  $log.log("inside token length")
+    //  var tken = authenticationFact.getToken()
+    //  authenticationFact.queryData(tken)
+    //  $scope.accountInfo = authenticationFact.getData();
+    //  console.log($scope.accountInfo.images[0]);
+    //}
 
 
 
 
 
-      Spotify.getCurrentUser().then(function (data) {
-        console.log(data);
-      });
-
-      $scope.menuOptions = [
-        {name: 'Search', link:'#/app/search', class: 'item-dark'},
-        {name: 'Browse', link: '#/app/browse', class: 'item-dark'},
-        {name: 'Account', link: '#/app/account', class: 'item-dark'},
-        {name: 'Playlists', link: '#/app/playlists', class: 'item-dark'}];
 
 
 
+    //$scope.$on('$stateChangeStart', function($q) {
+    //  // Code you want executed every time view is opened
+    //  currentState = $state.current.data.link;
+    //  $log.log("currentState: ", currentState)
+    //
+    //  if(!authenticationFact.isAuthorized()){
+    //    var tken = authenticationFact.getToken()
+    //    var promise =authenticationFact.queryData(tken);
+    //    promise.then(function(response){
+    //      $log.log(response)
+    //    })
+    //  }
+    //
+    //})
 
-      $scope.className = function(name){
-        var className = 'item-dark';
 
-        if (currentState === name){
-          className = 'item-royal';
-        }
 
-        return className;
+
+
+    Spotify.getCurrentUser().then(function (data) {
+      console.log(data);
+    });
+
+    $scope.menuOptions = [
+      {name: 'Search', link:'#/app/search', class: 'item-dark'},
+      {name: 'Browse', link: '#/app/browse', class: 'item-dark'},
+      {name: 'Account', link: '#/app/account', class: 'item-dark'},
+      {name: 'Playlists', link: '#/app/playlists', class: 'item-dark'}];
+
+
+
+
+    $scope.className = function(name){
+      var className = 'item-dark';
+
+      if (currentState === name){
+        className = 'item-royal';
+      }
+
+      return className;
     };
 
-    })
+  })
 
-    .controller('PlaylistsCtrl', function($scope, playlistsFact) {
-      //$scope.playlists = [
-      //  { title: 'Reggae', id: 1 },
-      //  { title: 'Chill', id: 2 },
-      //  { title: 'Dubstep', id: 3 },
-      //  { title: 'Indie', id: 4 },
-      //  { title: 'Rap', id: 5 },
-      //  { title: 'Cowbell', id: 6 }
-      //];
-      $scope.isDownloadActive = false;
+  .controller('PlaylistsCtrl', function($scope, playlistsFact) {
+    //$scope.playlists = [
+    //  { title: 'Reggae', id: 1 },
+    //  { title: 'Chill', id: 2 },
+    //  { title: 'Dubstep', id: 3 },
+    //  { title: 'Indie', id: 4 },
+    //  { title: 'Rap', id: 5 },
+    //  { title: 'Cowbell', id: 6 }
+    //];
+    //$scope.isDownloadActive = false;
 
-      $scope.playlist = playlistsFact.getPlaylist();
-
-
-
-
-    })
-  .controller('PlaylistCtrl', function($scope, $stateParams, $log, playlistsFact) {
-    $scope.isDownloadActive = false;
-    $scope.song = playlistsFact.getSong($stateParams.playlistId);
-    $log.log($scope.song)
-    //$scope.song = $scope.songs[0]
-
+    $scope.playlists = playlistsFact.getPlaylistsData();
+    $log.log("Playlists List: ", playlists[0].name);
 
 
 
 
   })
+  //.controller('PlaylistCtrl', function($scope, $stateParams, $log, playlistsFact) {
+  //  $scope.isDownloadActive = false;
+  //  $scope.playlists = playlistsFact.getPlaylists();
+  //  $log.log($scope.playlists)
+  //  //$scope.song = $scope.songs[0]
+  //
+  //
+  //
+  //
+  //
+  //})
 
-    .controller('login', function($scope, $stateParams, $log, $firebaseArray, $ionicPlatform, $ionicPopup, authenticationFact){
-      $scope.platform = ionic.Platform.platform();
+  .controller('login', function($scope, $stateParams, $log, $firebaseArray, $ionicPlatform, $ionicPopup, authenticationFact){
+    $scope.platform = ionic.Platform.platform();
 
-      $scope.performLogin = function(){
-        authenticationFact.login()
-        //https://accounts.spotify.com/authorize
-      }
+    $scope.performLogin = function(){
+      authenticationFact.login()
+      //https://accounts.spotify.com/authorize
+    }
 
 
 
-    });
+  });
 
 
 //http://10.31.23.184:8100
